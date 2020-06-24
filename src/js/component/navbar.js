4389754ef786c0d0c13a97f01f64e254799b3d90
Violeta Ventura <violetaventura@gmail.com>
2020-06-24 22:37:28 +0000

    nav bar icons

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,27 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import FormControl from "react-bootstrap/FormControl";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-export const Navbar = () => {
+export const NavBar = () => {
 	return (
-		<nav className="navbar navbar-dark bg-dark mb-3">
-			<button
-				className="navbar-toggler"
-				type="button"
-				data-toggle="collapse"
-				data-target="#navbarToggleExternalContent"
-				aria-controls="navbarToggleExternalContent"
-				aria-expanded="false"
-				aria-label="Toggle navigation">
-				<span className="navbar-toggler-icon" />
-			</button>
-			<Link to="/">
-				<span className="navbar-brand mb-0 text-white h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-warning">Donate</button>
-				</Link>
-			</div>
-		</nav>
+		<Navbar bg="dark" expand="dark" variant="dark">
+			<Navbar.Brand href="#home">HOMELESS AND HEROES</Navbar.Brand>
+			<Button variant="warning">
+				<i className="fas fa-user-circle fa-3x mr-auto" />
+			</Button>
+			<Button variant="warning" size="lg">
+				DONATE
+			</Button>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="mr-auto">
+					<NavDropdown title="Dropdown" id="basic-nav-dropdown">
+						<NavDropdown.Item href="#home">HOME</NavDropdown.Item>
+						<NavDropdown.Item href="#action/3.2">LOG IN</NavDropdown.Item>
+						<NavDropdown.Item href="#action/3.3">HOW WE ARE</NavDropdown.Item>
+						<NavDropdown.Item href="#action/3.4">HOW IT WORKS</NavDropdown.Item>
+						<NavDropdown.Item href="#action/3.5">GET INVOLVED</NavDropdown.Item>
+						<NavDropdown.Item href="#action/3.6">OUR PARTNERS</NavDropdown.Item>
+						<NavDropdown.Divider />
+						<NavDropdown.Item href="#action/3.7">
+							<i className="fab fa-facebook fa-3x" />
+							<i className="fab fa-twitter-square fa-3x" />
+							<i className="fab fa-instagram fa-3x" />
+						</NavDropdown.Item>
+					</NavDropdown>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
 	);
 };
