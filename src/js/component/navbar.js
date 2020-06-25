8eae0c161d58ad8f90cb5a7fd46149ce5885205c
Violeta Ventura <violetaventura@gmail.com>
2020-06-25 01:05:35 +0000

    connected user to demo view

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,34 +6,40 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import FormControl from "react-bootstrap/FormControl";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import ListGroup from "react-bootstrap/ListGroup";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 export const NavBar = () => {
 	return (
 		<Navbar bg="dark" expand="dark" variant="dark">
-			<Navbar.Brand href="#home">HOMELESS AND HEROES</Navbar.Brand>
-			<Button variant="warning">
-				<i className="fas fa-user-circle fa-3x mr-auto" />
-			</Button>
-			<Button variant="warning" size="lg">
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Brand href="#home" className="mr-auto p-3">
+				HOMELESS AND HEROES
+			</Navbar.Brand>
+			<Link to="/demo">
+				<Button variant="light " className="d-flex justify-content-end mr-3">
+					<i className="fas fa-user fa-3x" />
+				</Button>
+			</Link>
+			<Button bg="warning" variant="warning" size="xxl" type="text-white" className="p-3">
 				DONATE
 			</Button>
-			<Navbar.Toggle aria-controls="basic-navbar-nav" />
 			<Navbar.Collapse id="basic-navbar-nav">
 				<Nav className="mr-auto">
-					<NavDropdown title="Dropdown" id="basic-nav-dropdown">
-						<NavDropdown.Item href="#home">HOME</NavDropdown.Item>
-						<NavDropdown.Item href="#action/3.2">LOG IN</NavDropdown.Item>
-						<NavDropdown.Item href="#action/3.3">HOW WE ARE</NavDropdown.Item>
-						<NavDropdown.Item href="#action/3.4">HOW IT WORKS</NavDropdown.Item>
-						<NavDropdown.Item href="#action/3.5">GET INVOLVED</NavDropdown.Item>
-						<NavDropdown.Item href="#action/3.6">OUR PARTNERS</NavDropdown.Item>
-						<NavDropdown.Divider />
-						<NavDropdown.Item href="#action/3.7">
-							<i className="fab fa-facebook fa-3x" />
-							<i className="fab fa-twitter-square fa-3x" />
-							<i className="fab fa-instagram fa-3x" />
-						</NavDropdown.Item>
-					</NavDropdown>
+					<Link to="/">
+						<ListGroup.Item href="#home">HOME</ListGroup.Item>
+					</Link>
+					<ListGroup.Item href="#action/3.1">LOG IN</ListGroup.Item>
+					<ListGroup.Item href="#action/3.2">HOW WE ARE</ListGroup.Item>
+					<ListGroup.Item href="#action/3.3">HOW IT WORKS</ListGroup.Item>
+					<ListGroup.Item href="#action/3.4">GET INVOLVED</ListGroup.Item>
+					<ListGroup.Item href="#action/3.5">OUR PARTNERS</ListGroup.Item>
+					<NavDropdown.Divider />
+					<ListGroup.Item href="#action/3.6">
+						<i className="fab fa-facebook fa-3x p-3" />
+						<i className="fab fa-twitter-square fa-3x p-3" />
+						<i className="fab fa-instagram fa-3x p-3" />
+					</ListGroup.Item>
 				</Nav>
 			</Navbar.Collapse>
 		</Navbar>
